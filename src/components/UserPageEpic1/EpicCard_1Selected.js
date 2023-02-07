40986d3d72875d593632c5d3e63710bdda0d162a
Tou Fue Yang <touyangbiz@gmail.com>
2023-02-07 11:56:09 -0600

    this should of been on the last commit

--- src/components/UserPageEpic1/EpicCard_1Selected.js
+++ src/components/UserPageEpic1/EpicCard_1Selected.js
@@ -11,15 +11,9 @@ function Card1Selected() {
     return (
         <div className="container">
             <div>
-                <p>
-                    User's six samurai story will appear here if they created one.<br></br>
-                    A get.route will be placed here to fetch all fixed stories<br></br>
-                    Each picture/text will have a onClick to bring user to edit mode. 
-                </p>
                 <p>card_1: Shi-en confronts his father's killer.</p>
                 <button>Edit this post</button>
                 <button>Delete this Six Samurai EPIC</button>
-                <p>the^^ Add button will add picture to the epic </p>
                 <button onClick={() => history.push('/epic1')}>
                     Go Back Six Samurai Epic 
                 </button>
